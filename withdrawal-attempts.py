e14e3264e919a2ed2f2f6ca5cd0108c968fde41c
tbw875 <charger875@gmail.com>
2022-05-13 14:57:11 -0700

    init withdrawal attempts

---
+++ withdrawal-attempts.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = "https://api.chainalysis.com/api/kyt/v2/users/User875/withdrawal-attempts"
+
+payload = {
+    "network":"Ethereum"
+    ,"asset":"ETH"
+    ,"address":"0x92193107FB10B3B372AB21cC90b5a4DBd67861d9"}
+
+headers = {
+  'token': '06094f6ef2909569d7533ecd8706cad35703709de49713299d6e74edb937b878',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response)
