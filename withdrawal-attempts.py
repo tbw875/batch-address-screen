4c54cd322459a1ebd35d530047f04899010de876
tbw875 <charger875@gmail.com>
2022-05-16 12:16:16 -0700

    api call is working

--- withdrawal-attempts.py
+++ withdrawal-attempts.py
@@ -1,18 +1,14 @@
 import requests
 import json
 
-url = "https://api.chainalysis.com/api/kyt/v2/users/User875/withdrawal-attempts"
+url = "https://api.chainalysis.com/api/kyt/v1/users/User875/withdrawaladdresses"
 
-payload = {
-    "network": "Ethereum",
+payload = json.dumps([
+  {
     "asset": "ETH",
-    "address": "0x92193107FB10B3B372AB21cC90b5a4DBd67861d9",
-    "attemptIdentifier": "attempt1", 
-    "assetAmount": 0,
-    "assetPrice": 0,
-    "assetDenomination": "USD",
-    "attemptTimestamp": "2021-05-13T17:25:40.008307"
-}
+    "address": "0x92193107FB10B3B372AB21cC90b5a4DBd67861d9"
+  }
+])
 
 headers = {
   'token': '06094f6ef2909569d7533ecd8706cad35703709de49713299d6e74edb937b878',
@@ -21,4 +17,4 @@ headers = {
 
 response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response)
+print(response.text)
